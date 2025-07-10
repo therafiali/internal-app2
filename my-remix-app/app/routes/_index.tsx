8551675dac1b6b049c93e31eea622bec1ce30476
Muhammad Rafi Ali <therafiali@gmail.com>
2025-07-10 15:33:58 +0500

    updated

---
+++ my-remix-app/app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import { json, useLoaderData } from "@remix-run/react";
+
+ 
+// functional component
+
+export default function Index() {
+  
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
+}
