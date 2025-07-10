88d996f94e3096eb3dcc35066458623ec6f8f852
Muhammad Rafi Ali <therafiali@gmail.com>
2025-07-10 15:37:06 +0500

    feat - routing

---
+++ my-remix-app/app/routes/auth.signin.tsx
@@ -0,0 +1,11 @@
+import { json, useLoaderData } from "@remix-run/react";
+
+// functional component
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Signinpage</h1>
+    </div>
+  );
+}
