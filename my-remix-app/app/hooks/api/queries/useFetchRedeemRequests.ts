bd80ae9efdd43a12bf7fed2ae501664cac3dc119
Abdullah Memon <abdullahgulammemon76@gmail.com>
2025-07-16 22:40:46 -0700

    add redeem hooks

--- my-remix-app/app/hooks/api/queries/useFetchRedeemRequests.ts
+++ my-remix-app/app/hooks/api/queries/useFetchRedeemRequests.ts
@@ -36,9 +36,40 @@ async function fetchRechargeRequests(process_status: RechargeProcessStatus): Pro
   return data as RechargeRequest[];
 }
 
-export function useFetchRechargeRequests(process_status: RechargeProcessStatus) {
-  return useQuery<RechargeRequest[], Error>({
-    queryKey: ['recharge_requests', process_status],
-    queryFn: () => fetchRechargeRequests(process_status),
+
+async function fetchRedeemRequestsMultiple(process_statuses: string[]): Promise<RedeemRequest[]> {
+  const { data, error } = await supabase
+    .from('redeem_requests')
+    .select(`
+      *,
+      players:player_id (
+        firstname,
+        lastname
+      ),
+      payment_methods:payment_methods_id (
+        payment_method
+      ),
+      teams:team_id (
+        page_name
+      )
+    `)
+    .in('process_status', process_statuses);
+  console.log(data, 'redeem data multiple');
+  if (error) throw error;
+  return data as RedeemRequest[];
+}
+
+export function useFetchRedeemRequests(process_status: string) {
+  return useQuery<RedeemRequest[], Error>({
+    queryKey: ['redeem_requests', process_status],
+    queryFn: () => fetchRedeemRequests(process_status),
   });
-} 
+}
+
+export function useFetchRedeemRequestsMultiple(process_statuses: string[]) {
+  return useQuery<RedeemRequest[], Error>({
+    queryKey: ['redeem_requests_multiple', process_statuses],
+    queryFn: () => fetchRedeemRequestsMultiple(process_statuses),
+  });
+}
+
