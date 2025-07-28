212253810ebe1fef6f931479172b61976664f428
Muhammad Rafi Ali <therafiali@gmail.com>
2025-07-28 17:14:49 +0500

    Update useFetchTeams.ts

--- my-remix-app/app/hooks/api/queries/useFetchTeams.ts
+++ my-remix-app/app/hooks/api/queries/useFetchTeams.ts
@@ -14,7 +14,7 @@ async function fetchTeams(): Promise<string[]> {
     .select("team_code")
     .order("team_code", { ascending: true });
 
-  if (error) throw error;
+  if (error) throw error; 
 
   // Transform to array of team codes with "All Teams" prepended and make uppercase
   const teamCodes = data?.map((team) => team.team_code.toUpperCase()) || [];
